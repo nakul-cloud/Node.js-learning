f09bd123126fdda30c0ee929f6ad6f2d48df9706
Nakul Jadhav <jadhavnakul456@gmail.com>
2026-04-20 13:31:39 +0530

    add: implement user controller with CRUD operations and validation

--- assignment1/routes/usersRouter.js
+++ assignment1/routes/usersRouter.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-const controller = require("../controllers.js/usersController");
+const controller = require("../controllers/usersController");
 
 const router = Router();
 
