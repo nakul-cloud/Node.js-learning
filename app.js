4348a0aac22eac70affac44d447db2598d348017
Nakul Jadhav <jadhavnakul456@gmail.com>
2026-04-20 10:35:07 +0530

    initialize express application with basic setup

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const indexRouter = require("./routes/indexRouter");
+
+ 
+app.use(express.urlencoded({ extended: true }));
+ 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+ 
+app.use("/", indexRouter);
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
