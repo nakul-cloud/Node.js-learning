76516ff3beceef43b3f37a44f7092fe97faff9c5
Nakul Jadhav <jadhavnakul456@gmail.com>
2026-04-20 15:03:55 +0530

    add: initialize Express app and set up category router

---
+++ Project-2/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const categoryRouter = require("./routes/categoryRouter");
+
+app.use(express.urlencoded({ extended: true }));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use("/", categoryRouter);
+
+app.listen(3000, () => console.log("Server running"));
