b616e701e49636312b9f0218bf11cc8d01af8664
Nakul Jadhav <jadhavnakul456@gmail.com>
2026-04-20 13:01:50 +0530

    add app.js to set up Express server and routing

---
+++ assignment1/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const router = require("./routes/usersRouter");
+
+app.use(express.urlencoded({ extended: true }));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use("/", router);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
