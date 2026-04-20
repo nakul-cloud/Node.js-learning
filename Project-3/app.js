3d62e5c145f71d93bdbd7e68eda9a25b544eb592
Nakul Jadhav <jadhavnakul456@gmail.com>
2026-04-20 17:48:50 +0530

    add: implement passport authentication strategy with user serialization and deserialization

--- Project-3/app.js
+++ Project-3/app.js
@@ -3,6 +3,8 @@ const path = require("path");
 
 const session = require("express-session");
 const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcryptjs");
 
 const authRouter = require("./routes/authRouter");
 const messageRouter = require("./routes/messageRouter");
@@ -23,6 +25,7 @@ app.use(session({
   saveUninitialized: false
 }));
 
+app.use(passport.initialize());
 app.use(passport.session());
 
 // ROUTES
@@ -32,6 +35,38 @@ app.use("/user", userRouter);
 
 // HOME
 const pool = require("./db/pool");
+
+passport.use(new LocalStrategy(async (username, password, done) => {
+  try {
+    const { rows } = await pool.query(
+      "SELECT * FROM users WHERE username = $1",
+      [username]
+    );
+
+    const user = rows[0];
+    if (!user) return done(null, false);
+
+    const matches = await bcrypt.compare(password, user.password);
+    if (!matches) return done(null, false);
+
+    return done(null, user);
+  } catch (err) {
+    return done(err);
+  }
+}));
+
+passport.serializeUser((user, done) => done(null, user.id));
+passport.deserializeUser(async (id, done) => {
+  try {
+    const { rows } = await pool.query(
+      "SELECT * FROM users WHERE id = $1",
+      [id]
+    );
+    return done(null, rows[0] || null);
+  } catch (err) {
+    return done(err);
+  }
+});
 
 app.get("/", async (req, res) => {
   const { rows } = await pool.query(
