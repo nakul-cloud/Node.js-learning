8f73ede123bdb6c50a7192ffee2de04edfbf1829
Nakul Jadhav <jadhavnakul456@gmail.com>
2026-04-20 10:47:39 +0530

    initialize  app.js

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ app.use(express.urlencoded({ extended: true }));
  
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
- 
 app.use("/", indexRouter);
 
 app.listen(3000, () => {
