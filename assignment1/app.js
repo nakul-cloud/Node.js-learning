ba348e12f721b32ca519c717fc8cdb9a57b95eca
Nakul Jadhav <jadhavnakul456@gmail.com>
2026-04-20 13:32:37 +0530

    fix: correct views directory path in app.js

--- assignment1/app.js
+++ assignment1/app.js
@@ -6,7 +6,7 @@ const router = require("./routes/usersRouter");
 
 app.use(express.urlencoded({ extended: true }));
 
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "view"));
 app.set("view engine", "ejs");
 
 app.use("/", router);
