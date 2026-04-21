1cf23641215f00e8f606823652972dd9234ed6a9
Nakul Jadhav <jadhavnakul456@gmail.com>
2026-04-21 10:51:42 +0530

    feat: add authentication controller with signup and login functionality

---
+++ Project-4/controllers/authController.js
@@ -0,0 +1,36 @@
+const prisma = require("@prisma/client").PrismaClient;
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const client = new prisma();
+
+// SIGNUP
+exports.signup = async (req, res) => {
+  const hashed = await bcrypt.hash(req.body.password, 10);
+
+  const user = await client.user.create({
+    data: {
+      username: req.body.username,
+      password: hashed
+    }
+  });
+
+  res.json(user);
+};
+
+// LOGIN
+exports.login = async (req, res) => {
+  const user = await client.user.findUnique({
+    where: { username: req.body.username }
+  });
+
+  if (!user) return res.status(401).json("No user");
+
+  const match = await bcrypt.compare(req.body.password, user.password);
+
+  if (!match) return res.status(401).json("Wrong password");
+
+  const token = jwt.sign({ id: user.id }, "secret123");
+
+  res.json({ token });
+};
